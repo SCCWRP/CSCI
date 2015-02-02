0a80d17117725f26c630e84f0849acd736db709b
unknown <raphaelm@Raphael.SCCWRP2k.lan>
2015-02-02 13:58:33 -0800

    Updated helpfile for loadRefData()

--- R/loadRefData.r
+++ R/loadRefData.r
@@ -1,6 +1,8 @@
 #' Read in reference data used to build CSCI models
 #' @export
-#' 
+#' @details
+#' This function loads a data frame containing all reference sites used to calibrate the CSCI, including their sample date, 
+#' biotic group ID, O, E, O/E, MMI scores, observed metrics, predicted metrics, and scored metrics.
 loadRefData <- function(){
   read.csv(system.file("Data", "refsamples.csv", package="CSCI"))
 }
