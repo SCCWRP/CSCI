81ca37fadc23317af4eab8b21dfb1f010ea2ed6a
fawda123 <mbafs2012@gmail.com>
2019-03-14 14:08:23 -0700

    updated refsamples with validation sites

--- R/loadRefData.r
+++ R/loadRefData.r
@@ -1,7 +1,7 @@
 #' Read in reference data used to build CSCI models
 #' @export
 #' @details
-#' This function loads a data frame containing all reference sites used to calibrate the CSCI, including their sample date, 
+#' This function loads a data frame containing all reference sites used to calibrate and validate the CSCI, including their sample date, 
 #' biotic group ID, O, E, O/E, MMI scores, observed metrics, predicted metrics, scored metrics, and PSA regions.
 loadRefData <- function(){
   refsamples
