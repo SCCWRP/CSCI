2d9d72b89f13ca324216055ea70eebdd59e509e8
Addison Torres Grant <addisong@sccwrp.org>
2025-07-22 17:03:51 -0700

    Sample the output data so the snapshots are smaller
    This brings them down from a few hundred KB to less than 60 KB.

--- tests/testthat/test-refprox.R
+++ tests/testthat/test-refprox.R
@@ -1,15 +1,27 @@
 test_that("refprox function generates expected data for bug_stations[[2]][1, ]", {
   results = refprox(bugs_stations[[2]][1, ], output = "dat")
+  # Set a random seed for reproducibility of slice_sample below
+  set.seed(1)
+  # Get a subset of the output so the snapshot is not too large
+  results = results |> dplyr::slice_sample(n = 25)
   expect_snapshot(results)
 })
 
 test_that("refprox function generates expected data for bug_stations[[2]][2, ]", {
   results = refprox(bugs_stations[[2]][2, ], output = "dat")
+  # Set a random seed for reproducibility of slice_sample below
+  set.seed(1)
+  # Get a subset of the output so the snapshot is not too large
+  results = results |> dplyr::slice_sample(n = 25)
   expect_snapshot(results)
 })
 
 test_that("refprox function generates expected data for bug_stations[[2]][3, ]", {
   results = refprox(bugs_stations[[2]][3, ], output = "dat")
+  # Set a random seed for reproducibility of slice_sample below
+  set.seed(1)
+  # Get a subset of the output so the snapshot is not too large
+  results = results |> dplyr::slice_sample(n = 25)
   expect_snapshot(results)
 })
 
